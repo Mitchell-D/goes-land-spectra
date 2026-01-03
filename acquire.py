fixed some results calculation problems and further verified results.

acquire.py
@@ -363,7 +363,7 @@ def get_goes_l1b_and_masks(geom_dir:Path, bucket:str, listing:list,
                 rad_results[rkey]["m4"][m_first] = 0.
 
             ## substitute minimum and maximum
-            rad_results[rkey]["max"][m_sub] = np.nanmin(
+            rad_results[rkey]["max"][m_sub] = np.nanmax(
                     [rad_results[rkey]["max"][m_sub], sub_rad], axis=0)
             rad_results[rkey]["min"][m_sub] = np.nanmin(
                     [rad_results[rkey]["min"][m_sub], sub_rad], axis=0)
@@ -633,8 +633,8 @@ if __name__=="__main__":
     ## start and end day for data listing
     #sday,eday,gver = datetime(2018,1,1),datetime(2022,12,31) ## test period
 
-    sday,eday,gver = datetime(2017,7,1),datetime(2024,6,30),16 ## 7y 16E
-    #sday,eday,gver = datetime(2019,1,1),datetime(2021,12,31),17 ## 3y 17W
+    #sday,eday,gver = datetime(2017,7,1),datetime(2024,6,30),16 ## 7y 16E
+    sday,eday,gver = datetime(2019,1,1),datetime(2021,12,31),17 ## 3y 17W
     #sday,eday,gver = datetime(2022,7,1),datetime(2025,6,30),18 ## 3y 18W
     #sday,eday,gver = datetime(2024,10,15),datetime(2025,3,15),19 ## <1y 19C
 
@@ -643,6 +643,7 @@ if __name__=="__main__":
 
     ## UTC hours of the day to capture shortwave and longwave radiances
     swtimes = [timedelta(hours=t) for t in [12,15,18,21,0]]
+    #swtimes = [timedelta(hours=t) for t in [15,18,21]]
     #lwtimes = [timedelta(hours=t) for t in [12,15,18,21,0,3,6,9]]
     lwtimes = swtimes ## for now, no night pixels. need night-specific masking
 
@@ -667,7 +668,7 @@ if __name__=="__main__":
 
     ## on meteor, head calc:~6.5s sometimes, worker ~5e6 px/sec NOT inc. DL
     #nworkers,batch_size = 8,24 ##
-    nworkers,batch_size = 8,8 ##
+    nworkers,batch_size = 6,24 ##
 
     ## identifying name of this dataset for the listing pkl
     listing_name = f"goes{gver}_clearland-l1b-c0" ## lmask&l1b combo 0
@@ -747,7 +748,7 @@ if __name__=="__main__":
     ## is critical for the head/worker balance's efficiency.
     listing = sorted(
             list(filter(lambda l:l[0] not in loaded, listing)),
-            key=lambda l:(l[0][1],l[0][0][4:6],)
+            key=lambda l:(l[0][0][4:6],l[0][1]) ## time of day then month
             )
 
     batches = len(listing) // batch_size + bool(len(listing) % batch_size)
@@ -766,6 +767,8 @@ if __name__=="__main__":
     results = {} ## rkey:(geom,month,tod,band)
     if not overwrite_results:
         for p in out_dir.iterdir():
+            if "acquire" in p.name:
+                continue
             sat,lk,ts0,tsf,gstr,mstr,sstr,bstr = p.stem.split("_")
             rkey = (gstr,mstr,sstr,bstr)
             tmp_lkey = "_".join((sat,lk,ts0,tsf))
@@ -791,7 +794,6 @@ if __name__=="__main__":
             ## for each of the results returned, merge the welford dict with
             ## that of previously loaded results. Unique results are identified
             ## by keys that specify the (geom, month, ToD, band) combo
-            print(f"head got rkeys: ", "\n".join(map(str,tmp_res.keys())))
             for rkey in tmp_res.keys():
                 out_path = out_dir.joinpath("_".join([lkey,*rkey])+".pkl")
                 cur = tmp_res[rkey]

helpers.py
@@ -240,7 +240,6 @@ def merge_welford(w1, w2):
     new = {
         "count":np.full(w1["count"].shape, 0, dtype=np.float32),
         "min":np.full(w1["count"].shape, np.nan, dtype=np.float32),
-        "min":np.full(w1["count"].shape, np.nan, dtype=np.float32),
         "max":np.full(w1["count"].shape, np.nan, dtype=np.float32),
         "m1":np.full(w1["count"].shape, np.nan, dtype=np.float32),
         "m2":np.full(w1["count"].shape, np.nan, dtype=np.float32),
@@ -260,7 +259,7 @@ def merge_welford(w1, w2):
         new["count"][mv_only_w2] = w2["count"][mv_only_w2]
         new["min"][mv_only_w2] = w2["min"][mv_only_w2]
         new["max"][mv_only_w2] = w2["max"][mv_only_w2]
-        new["m2"][mv_only_w2] = w2["m1"][mv_only_w2]
+        new["m1"][mv_only_w2] = w2["m1"][mv_only_w2]
         new["m2"][mv_only_w2] = w2["m2"][mv_only_w2]
         new["m3"][mv_only_w2] = w2["m3"][mv_only_w2]
         new["m4"][mv_only_w2] = w2["m4"][mv_only_w2]